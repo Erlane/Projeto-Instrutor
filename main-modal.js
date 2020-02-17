0934cb425b4f9a329b3fa12926fedee5bb0dcd7d
Erlane Mendes <erlane.mendes31@gmail.com>
2020-02-17 15:27:46 -0300

    projeto quase terminado

---
+++ main-modal.js
@@ -0,0 +1,32 @@
+function iniciarModal() {
+	document.querySelector('.overlay-modal').style.display = 'flex';
+
+}
+
+function fecharModal(){
+	document.querySelector('.overlay-modal').style.display = 'none';
+
+}
+
+function modalVoluntario() {
+	document.querySelector('.modal-voluntario').style.display = 'flex';
+
+}
+
+function fechar() {
+	document.querySelector('.modal-voluntario').style.display = 'none';
+	
+}
+
+function confirmar() {
+	document.querySelector('.modal-confirmation').style.display = 'flex';
+
+}
+
+function fecharAgradecer() {
+	document.querySelector('.modal-confirmation').style.display = 'none';
+	
+}
+
+
+
--- main.js
+++ main.js
@@ -1,13 +1,19 @@
-function test (sessao) {
-	console.log('.' + sessao + '-1')
-	document.querySelector('.' + sessao + '-1').style.display = 'none';
-	document.querySelector('.' + sessao + '-2').style.display = 'flex';
+function test (sessionDog) {
+	console.log('.llllll')
+
+	console.log('.' + sessionDog + '-1')
+	document.querySelector('.' + sessionDog + '-1').style.display = 'none';
+	document.querySelector('.' + sessionDog + '-2').style.display = 'flex';
+	document.querySelector(container-p).style.display = 'none';
 
 }
 
-function test2 (sessao) {
-	console.log('.' + sessao + '-1')
-	document.querySelector('.' + sessao + '-2').style.display = 'none';
-	document.querySelector('.' + sessao + '-1').style.display = 'flex';
+function test2 (sessionDog) {
+	console.log('.llllcvc')
+
+	console.log('.' + sessionDog + '-1')
+	document.querySelector('.' + sessionDog + '-1').style.display = 'flex';
+	document.querySelector('.' + sessionDog + '-2').style.display = 'none';
 
 }
+
